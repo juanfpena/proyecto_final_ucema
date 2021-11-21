7800a1f235a79365ed2585c59e046995944fd134
juanfpena <62632100+juanfpena@users.noreply.github.com>
2021-11-21 13:45:39 -0300

    update

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="main_index")
+    path('', views.IndexView.as_view(), name='main_index'),
+    path('registrarse', views.RegistrarseView.as_view(), name='main_registrarse'),
+    path('producto/<int:pk>/', views.verProducto.as_view(), name='ver_producto'),
+    path('cargar_pedido', views.cargarPedido.as_view(), name='cargar_pedido')
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, UpdateView
 from .models import Cliente, Producto, Pedido
 from django.views import View
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
@@ -20,9 +21,33 @@ class IndexView(TemplateView):
 
 
 class RegistrarseView(TemplateView):
-    template_name = 'main/registrarse.html'
+    template_name = 'registrarse.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        return context
+
+
+class verProducto(TemplateView):
+    template_name = 'cargar_pedido.html'
+
+    def get(self, *args, **kwargs):
+        instance = get_object_or_404(Producto, pk=self.kwargs['pk'])
+
+        context = {'producto': instance}
+
+        return render(self.request, self.template_name, context)
+
+
+class cargarPedido(TemplateView):
+    template_name = 'producto.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        productos = Producto.objects.all()
+
+        context['productos'] = productos
 
         return context
